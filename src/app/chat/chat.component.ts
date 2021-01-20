1333801e350f324170ded1cc64e91ff69d66a46b
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-01-20 21:16:32 +0100

    feat: clear message and scroll after sent

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { uniqueId } from 'lodash';
 import { Observable } from 'rxjs';
 
 import { ChatMessage } from '../shared/chat-message';
@@ -11,7 +16,9 @@ import { DataService } from '../shared/data.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
+  @ViewChild('chatMessagesContainer') private chatMessagesContainer: ElementRef;
+
   messages$: Observable<ChatMessage[]>;
   private slug: string;
 
@@ -23,6 +30,17 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     this.slug = this.route.snapshot.paramMap.get('slug');
     this.messages$ = this.dataService.getChatMessages(this.slug);
+    this.scrollToBottom();
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.chatMessagesContainer.nativeElement.scrollTop = this.chatMessagesContainer.nativeElement.scrollHeight;
+    } catch (err) {}
   }
 
   sendMessage(message: string) {
